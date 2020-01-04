3d34771d05aaf7b3b5e93861d60a71d3c5606bca
Bradley Knutson <bradley@bknutson.com>
2020-01-04 11:45:51 -0700

    Added paths for html pages

--- app.js
+++ app.js
@@ -7,34 +7,42 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let tables = [
-    {
-        name: 'Nicholas',
-        phoneNumber: '8675309',
-        email: 'example@example.com',
-        uniqueId: 'beigerage'
-    }
-];
+let tables = [];
 
-let waitlist = [
-    {
-        name: 'Lucas',
-        phoneNumber: '6666666',
-        email: 'hitler@satan.com',
-        uniqueId: 'wtfdude'
-    }
-];
+let waitlist = [];
 
 app.get('/', (req, res) => {
-    res.send('This is working');
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get('/tables/', (req, res) => {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get('/reserve/', (req, res) => {
+    res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 app.get('/api/tables/', (req, res) => {
-    res.send(tables);
+    res.json(tables);
 });
 
-app.get('/api/waitlist', (req, res) => {
-    res.send(waitlist);
+app.get('/api/waitlist/', (req, res) => {
+    res.json(waitlist);
+});
+
+app.get('/api/tables/:table', (req, res) => {
+
+})
+
+app.post('/api/', (req, res) => {
+    if(tables.length < 5){
+        tables.push(req.body);
+        res.send('Successfully added to a table');
+    }else{
+        waitlist.push(req.body);
+        res.send('Successfully added to waitlist');
+    }
 });
 
 app.listen(PORT, () => {
